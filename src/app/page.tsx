a045041802bf3da7939050adfde69715c42252aa
zouker <goncalojbsousa@gmail.com>
2024-08-03 15:43:18 +0100

    deploy fix 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import HomePage from "@/src/app/home";
-import { checkIsAuthenticated } from "@/src/lib/auth/checkIsAuthenticated";
 
 export default async function Home() {
 
-  const isAuthenticated = await checkIsAuthenticated();
-
-
-  return <HomePage isAuthenticated={isAuthenticated} />;
+  return <HomePage />;
 
 }
