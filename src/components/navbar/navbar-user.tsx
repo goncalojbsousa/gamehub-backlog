aec507380dcef1fbcf80d234f0d421310c91d952
zouker <goncalojbsousa@gmail.com>
2024-08-27 23:43:20 +0100

    removed setting from navbar menu

--- src/components/navbar/navbar-user.tsx
+++ src/components/navbar/navbar-user.tsx
@@ -57,17 +57,19 @@ export const NavbarUser: React.FC<UserProps> = ({ usernameSlug, userImage }) => 
                 onClick={toggleMenu}
             />
             {menuOpen && (
-                <div ref={menuRef} className="absolute mt-52 mr-24 bg-color_sec shadow-md border border-border_detail rounded-md w-48 p-2">
+                <div ref={menuRef} className="absolute mt-36 mr-24 bg-color_sec shadow-md border border-border_detail rounded-md w-48 p-2"> {/* WITH SETTING MT IS 52 mt-52 */}
                     <Link href={`/user/${usernameSlug}`} className="flex px-4 py-2 text-color_text hover:bg-color_main rounded-md transition-colors duration-200 active:bg-color_click select-none">
                         <ProfileIcon />
                         Profile
                     </Link>
-                    <Link href="/settings" className="flex px-4 py-2 text-color_text hover:bg-color_main rounded-md transition-colors duration-200 active:bg-color_click select-none">
+                    {/*<Link href="/settings" className="flex px-4 py-2 text-color_text hover:bg-color_main rounded-md transition-colors duration-200 active:bg-color_click select-none">
                         <SettingsIcon />
                         Settings
                     </Link>
-                    <hr className="mt-2 mb-2" />
-                    <SignOutButton className="w-full flex justify-start itens-center px-4 py-2 text-color_text hover:bg-btn_logout rounded-md transition-colors duration-200 select-none">
+                    <hr className="mt-2 mb-2" />*/
+                    }
+                    
+                    <SignOutButton className="flex w-full px-4 py-2 text-color_text hover:bg-btn_logout rounded-md transition-colors duration-200 active:bg-color_click select-none">
                         <LogoutIcon />
                         Logout
                     </SignOutButton>
